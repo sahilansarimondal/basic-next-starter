7cf9e4894669e4f906628fc836611d6a696cd2e3
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2025-05-10 18:52:48 +0530

    added google Oauth and prisma orm with schema

---
+++ src/components/sign-in.tsx
@@ -0,0 +1,19 @@
+import { signIn } from "@/auth";
+
+export default function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <button
+        className=" border  p-2 rounded-2xl mt-4 cursor-pointer bg-slate-100"
+        type="submit"
+      >
+        Signin with Google
+      </button>
+    </form>
+  );
+}
