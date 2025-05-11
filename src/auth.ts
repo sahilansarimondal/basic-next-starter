b40db54eb7314088476e1575ed6e01848cc86d47
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2025-05-11 08:39:03 +0530

    added edge compatibility in next auth to use prisma adapter with auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,10 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
 import { prisma } from "./prisma";
+import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
